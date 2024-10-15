a0f921743fce2d67b0b28b39cee36860edb0d449
Abdullah Alhumaidhan <abdullahkh56@gmail.com>
2024-10-15 15:07:53 +0300

    First commit

---
+++ src/app/products.js
@@ -0,0 +1,23 @@
+import mascarpone from "./mascarpone.jpeg";
+import cinnamon from "./cinnamon.jpeg";
+import chocolate from "./chocolate.jpeg";
+
+const products = [
+  {
+    name: "Mascarpone Croissant",
+    price: "1.750 KD",
+    image: mascarpone,
+  },
+  {
+    name: "Cinnamon Roll",
+    price: "1.500 KD",
+    image: cinnamon,
+  },
+  {
+    name: "Chocolate Cruffin",
+    price: "2.000 KD",
+    image: chocolate,
+  },
+];
+
+export default products;
